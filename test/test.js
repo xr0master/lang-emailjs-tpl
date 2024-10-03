e520127bc142957a18ab51efc8c134d769341330
Sergey Khomushin <xr0master@gmail.com>
2024-10-03 16:56:58 +0800

    update packages

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ let caseDir = path.dirname(fileURLToPath(import.meta.url));
 for (let file of fs.readdirSync(caseDir)) {
   if (!/\.txt$/.test(file)) continue;
 
-  let name = /^[^\.]*/.exec(file)[0];
+  let name = /^[^.]*/.exec(file)[0];
   describe(name, () => {
     for (let { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file))
       it(name, () => run(emailjsTemplateLanguage.parser));
